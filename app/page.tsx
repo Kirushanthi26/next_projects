6c9d6817c160f07525e329b57ae7cfe80bdced30
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-04-09 12:06:34 +0530

    feat: initialize Next.js project with basic setup
    Add initial project files including Next.js configuration, layout, page, ESLint, TypeScript, and package.json. This sets up the foundation for a new Next.js application with essential dependencies and configurations.

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to my website</p>
+      <p>Here are some of my projects</p>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
